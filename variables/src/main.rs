8e2fafd6d2ffcb94264e047e3cb0b01834eb6a01
orothy579 <ftsky2050@naver.com>
2025-03-28 09:50:26 +0900

    aa

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -28,4 +28,43 @@ fn main() {
 
     let guess: u32 = "42".parse().expect("Not a number ");
     println!("{guess}");
+
+    let z = 2.3;
+    let q: f32 = 3.4;
+    // let w: u8 = 256;
+    println!("z: {z}, q: {q}");
+
+    let sum = 5 + 10;
+    println!("sum :  {sum}");
+
+    let diff = 95.5 - 3.4;
+    println!("diff : {diff}");
+
+    let prod = 4 * 30;
+    println!("prod : {prod}");
+
+    let quo = 56.7 / 32.2;
+    println!("quo : {quo}");
+
+    let tranc = -5 / 3;
+    println!("tranc : {tranc}");
+
+    let remainder = 43 % 5;
+    println!("ramainder : {remainder}");
+
+    let t = true;
+    let f = false;
+
+    println!("true : {t}");
+
+    let tup: (i32, f64, u8) = (5000, 6.3, 1);
+    let (x, y, z) = tup;
+    println!("x : {x} , y : {y}, z : {z}");
+    let tup_0 = tup.0;
+    let tup_1 = tup.1;
+    let tup_2 = tup.2;
+
+    println!("tup.0 : {tup_0} , tup.1 : {tup_1} , tup.2 : {tup_2}");
+
+    let a = [1, 2, 3, 4, 5];
 }
