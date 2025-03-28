90b0f2dedd2484550858791ebd4fb925725e8994
orothy579 <ftsky2050@naver.com>
2025-03-28 10:35:28 +0900

    del cargo

--- hello_cargo/target/debug/deps/hello_cargo-4516d1a582651c74.d
+++
@@ -1,5 +0,0 @@
-/Users/lch/development/rust/projects/hello_cargo/target/debug/deps/libhello_cargo-4516d1a582651c74.rmeta: src/main.rs
-
-/Users/lch/development/rust/projects/hello_cargo/target/debug/deps/hello_cargo-4516d1a582651c74.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/lch/development/rust/projects/hello_cargo/target/debug/hello_cargo: /Users/lch/development/rust/projects/hello_cargo/src/main.rs
