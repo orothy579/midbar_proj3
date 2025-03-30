b7a5444464cacbb0ba60c9d74687c0df3bfc45e4
orothy579 <ftsky2050@naver.com>
2025-03-30 22:44:30 +0900

    4-7

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -62,7 +62,12 @@
 // }
 
 // 예제 4-7
-fn main() {}
+fn main() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear();
+    println!("word is {word}")
+}
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
