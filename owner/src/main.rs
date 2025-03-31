e30ba75d0e5278b3245e02112a7155272a1434dc
orothy579 <ftsky2050@naver.com>
2025-03-31 13:02:14 +0900

    chap4

--- owner/src/main.rs
+++ owner/src/main.rs
@@ -101,40 +101,47 @@
 //     println!("the first word is : {}", word);
 // }
 
+// fn main() {
+//     let my_string = String::from("hello world");
+//     let word = first_word(&my_string[0..6]);
+//     println!("word is {word}");
+//     let word = first_word(&my_string[..]);
+//     println!("word is {word}");
+//     let word = first_word(&my_string);
+//     println!("word is {word}");
+
+//     let my_string_literal = "hello world";
+
+//     let word = first_word(&my_string_literal[0..6]);
+//     println!("word is {word}");
+
+//     let word = first_word(&my_string_literal[..]);
+//     println!("word is {word}");
+
+//     // let word = first_word(my_string_literal);
+//     // let word = first_word(&my_string);
+
+//     let a = [1, 2, 3, 4, 5];
+
+//     let slice = &a[1..3];
+
+//     assert_eq!(slice, &[2, 3], "we are testing");
+// }
+
+// fn first_word(s: &str) -> &str {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return &s[0..i];
+//         }
+//     }
+//     &s[..]
+// }
+
 fn main() {
-    let my_string = String::from("hello world");
-    let word = first_word(&my_string[0..6]);
-    println!("word is {word}");
-    let word = first_word(&my_string[..]);
-    println!("word is {word}");
-    let word = first_word(&my_string);
-    println!("word is {word}");
+    let mut s = String::from("hello");
+    s.push_str(", world");
 
-    let my_string_literal = "hello world";
-
-    let word = first_word(&my_string_literal[0..6]);
-    println!("word is {word}");
-
-    let word = first_word(&my_string_literal[..]);
-    println!("word is {word}");
-
-    // let word = first_word(my_string_literal);
-    // let word = first_word(&my_string);
-
-    let a = [1, 2, 3, 4, 5];
-
-    let slice = &a[1..3];
-
-    assert_eq!(slice, &[2, 3], "we are testing");
-}
-
-fn first_word(s: &str) -> &str {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return &s[0..i];
-        }
-    }
-    &s[..]
+    println!("{s}");
 }
