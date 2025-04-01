38fcec5a8f7cd91120a0ad23e92d2a1f4954fb0a
orothy579 <ftsky2050@naver.com>
2025-04-01 17:21:09 +0900

    meeting3

--- meeting2/src/main.rs
+++ meeting2/src/main.rs
@@ -1,5 +1,3 @@
-use core::borrow;
-
 fn main() {
     //move
     let s = String::from("Hello");
@@ -12,37 +10,39 @@ fn main() {
     // let q = i;
     // println!("{q}");
 
-    // 대여
-    // let mut b = 10;
-    // let copy_b = copy_function(b);
-    // println!("b is {b}");
-    // println!("modified b is {copy_b}");
+    //대여
+    let mut b = 10;
+    let copy_b = copy_function(b);
+    println!("b is {b}");
+    println!("modified b is {copy_b}");
 
-    // let borrow_b = borrow_function(&mut b);
-    // println!("b is {b}");
-    // println!("modified b is {borrow_b}");
+    let borrow_b = borrow_function(&mut b);
+    println!("b is {b}");
+    println!("modified b is {borrow_b}");
 
-    let mut str = String::from("hello");
-    {
-        let str1 = &str;
-        let str2 = &str;
+    // 함수 없이 참조하기
+    // let mut str = String::from("hello");
 
-        println!("{} , {}", str1, str2);
+    // {
+    //     let str1 = &str;
+    //     let str2 = &str;
 
-        let str3 = &mut str;
-        str3.push_str(", world");
-        println!("{str3} ,{str1}");
-    }
+    //     println!("{} , {}", str1, str2);
+
+    //     let str3 = &mut str;
+    //     str3.push_str(", world");
+    //     println!("{str3} ,{str1}");
+    // }
 }
 
-fn copy_function(mut b: i32) -> i32 {
-    b = b + 1;
-    println!("{}", b);
-    b
-}
+// fn copy_function(mut b1: i32) -> i32 {
+//     b1 = b1 + 1;
+//     println!("{}", b1);
+//     b1
+// }
 
-fn borrow_function(b: &mut i32) -> i32 {
-    *b = *b + 1;
-    println!("{}", *b);
-    *b
-}
+// fn borrow_function(b1: &mut i32) -> i32 {
+//     *b1 = *b1 + 1;
+//     println!("{}", *b1);
+//     *b1
+// }
