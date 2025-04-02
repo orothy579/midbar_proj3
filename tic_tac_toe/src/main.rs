51227eecba31580d8907bb85eae4a89ac4cf3bdc
orothy579 <ftsky2050@naver.com>
2025-04-02 15:45:05 +0900

    aa

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -9,8 +9,8 @@ struct Player {
 
 #[derive(PartialEq, Clone)]
 struct Position {
-    row: u8,
-    col: u8,
+    row: i32,
+    col: i32,
 }
 
 fn main() {
@@ -30,14 +30,14 @@ fn main() {
 
     // 초기화
     let mut current_player = player1.clone();
-    let mut board: [[&str; 3]; 3] = [[" "; 3]; 3];
+    let mut board: [[char; 3]; 3] = [[' '; 3]; 3];
 
     // 게임
-    'main_loop: loop {
+    loop {
         // 1.보드 출력
         print_board(&board);
-        // 2. 사용자 입력
 
+        // 2. 사용자 입력
         let mut row = String::new();
         let mut col = String::new();
 
@@ -51,38 +51,44 @@ fn main() {
             .read_line(&mut col)
             .expect("Failed to read line");
 
-        let row: usize = match row.trim().parse() {
+        let row: i32 = match row.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        let col: usize = match col.trim().parse() {
+        let col: i32 = match col.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        // // 3. 사용자 입력 vaildation check
-        // if position_is_valid(&board, &current_player.position) == false {
-        //     println!("Wrong position.. Try again.");
-        //     continue;
-        // }
+        let pos = Position { row, col };
 
-        // // 4. 유효한 경우 보드에 마커 그리기
-        // board[row][col] = mark_of(&current_player);
+        // 3. 사용자 입력 vaildation check
+        if position_is_valid(&board, &pos) == false {
+            println!("Wrong position.. Try again.");
+            continue;
+        }
 
-        // // 5. 승리 조건 체크
-        // if is_win() {
-        //     println!("You win!");
-        //     break;
-        // }
+        current_player.position = Position { row, col };
 
-        // // 6. 무승부 체크
-        // if is_draw() {
-        //     println!("draw!");
-        //     break;
-        // }
+        // 4. 유효한 경우 보드에 마커 그리기
+        board[row as usize][col as usize] = mark_of(&current_player);
+
+        // 5. 승리 조건 체크
+        if is_win(&board) == true {
+            println!("{:?}", board);
+            println!("You win!");
+            break;
+        }
+
+        // 6. 무승부 체크
+        if is_draw(&board) {
+            println!("draw!");
+            break;
+        }
 
         // 7. 플레이어 교체
+        // clone 말고 다른 방법으로 교체 예정 -> player 교체가 안된다.
         if current_player == player1 {
             current_player = player2.clone();
         } else {
@@ -91,20 +97,62 @@ fn main() {
     }
 }
 
-fn print_board(board: &[[&str; 3]; 3]) {
-    println!("{:#?}", board);
+fn print_board(board: &[[char; 3]; 3]) {
+    println!("{:?}", board);
 }
 
-fn position_is_valid(b: &String, p: &Position) -> bool {
-    return false;
-}
+// logic이 잘못된듯.. -> 한번 wrong positoin 걸리면 계속 wrong positoin 걸린다.
+fn position_is_valid(b: &[[char; 3]; 3], p: &Position) -> bool {
+    //  (-1, -1) 과 같이 음수에 접근하는 경우
+    //     -> false -> Question: 이미 타입이 u8인데, 음수가 받아지나?
+    if p.col < 0 || p.row > 2 {
+        return false;
+    }
+    //  이미 있는 포지션에 접근하는 경우 -> false
+    if b[p.row as usize][p.col as usize] != ' ' {
+        return false;
+    }
 
-fn mark_of(p: &Player) {}
-
-fn is_win() -> bool {
     return true;
 }
 
-fn is_draw() -> bool {
+fn mark_of(p: &Player) -> char {
+    // if board is full
+    if p.name == "player1" {
+        return 'X';
+    } else {
+        return 'O';
+    }
+}
+
+fn is_win(board: &[[char; 3]; 3]) -> bool {
+    let win_condition = [
+        [board[0][0], board[1][0], board[2][0]],
+        [board[1][1], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ];
+
+    for cond in win_condition {
+        if cond == ['X'; 3] || cond == ['O'; 3] {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn is_draw(b: &[[char; 3]; 3]) -> bool {
+    for b_row in b {
+        for e in b_row {
+            if *e != ' ' {
+                return false;
+            }
+        }
+    }
     return true;
 }
